c5c6dba3ff202f1c846253e36e9bf6c26e015615
Paul Foulont <paulfoulont@mbp-de-paul.home>
2024-11-14 21:08:29 +0100

    feat: add logs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class HomeController extends AbstractController
 {
     private $apiClient;
 
-    public function __construct(ApiClient $apiClient)
+    private $logger;
+
+    public function __construct(ApiClient $apiClient, LoggerInterface $logger)
     {
         $this->apiClient = $apiClient;
+        $this->logger = $logger;
     }
 
     #[Route('/', name: 'home')]
@@ -27,8 +31,20 @@ class HomeController extends AbstractController
         if (!empty($query)) {
             try {
                 $movies = $this->apiClient->getData('search/movie', ['query' => urlencode($query), 'page' => $page, 'language' => 'fr-FR']);
+
+                $this->logger->info('Results of movie search request for `' . urlencode($query) . '` at page ' . $page . ':', [
+                    'count' => count($movies['results']),
+                    'page' => $movies['page'],
+                    'total_pages' => $movies['total_pages'],
+                ]);
             } catch (\Exception $e) {
                 $this->addFlash('error', 'Une erreur est survenue lors de la recherche des films.');
+
+                $this->logger->error('Error when trying to search movies for `' . urlencode($query) . '` at page ' . $page . ':', [
+                    'exception' => $e->getMessage(),
+                    'query' => $query,
+                    'page' => $page,
+                ]);
             }
         }
 
